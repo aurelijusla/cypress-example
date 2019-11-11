695e376868299a446e034a9e3324adc0b1e00786
Aurelijus Lasys <aurelijus.lasys@gmail.com>
2019-11-11 23:19:33 +0200

    First test tryout

---
+++ cypress/integration/claim-form/claim-form.spec.js
@@ -0,0 +1,54 @@
+// TODOS:
+// Test 1. Direct flight, happy flow first iteration
+// beforeEach
+// refactor
+// Add a document (?)
+// Test 2
+// Test 3
+// Prettier
+
+describe('Claim Form test suite', () => {
+    it('Direct flight, happy flow', () => {
+        cy.visit('');
+        // 1 step. Flight itinerary
+        cy.contains('#airport-departure', 'Enter the city').click();
+        cy.get('.is-focused').type('vno');
+        cy.get('.Select-option').first().click();
+        cy.contains('#airport-arrival', 'Enter the city').click();
+        cy.get('.is-focused').type('arn');
+        cy.get('.Select-option').first().click();
+        // 2 step. Disruption details
+        cy.contains('button', 'Continue').click();
+        cy.contains('Flight cancelled').click();
+        cy.contains('Never arrived').click();
+        cy.contains('Less than 14 days').click();
+        cy.contains('button', 'Continue').click();
+        // 3 step. Disruption reason
+        cy.contains('Yes').click();
+        cy.contains('Other reasons').click();
+        cy.get('textarea').type('This is an automated test.');
+        cy.contains('button', 'Continue').click();
+        // 4 step. Flight details
+        cy.contains('Enter airlines name').type('sk');
+        cy.get('.Select-option').first().click();
+        cy.get('#flight-1-date').click();
+        cy.get('.rdtToday').click();
+        cy.get('#flight-1-number').type('1234');
+        cy.contains('button', 'Continue').click();
+        // 5 step. Documents
+        cy.contains('button', 'Continue').click();
+        // 6 step. Passenger information
+        cy.contains('Good news!').should('be.visible'); // Decided to end here, not to submit to prod db
+        // 7 step. Signature
+        // 8 step. Finish!
+    })
+})
+
+    // describe('Full claim direct flight, happy flow', () => {
+    //     Placeholder
+    // })
+
+    // describe('Full claim, flight not eligible for compensation', () => {
+    //     Placeholder
+    //       })
+    // })
